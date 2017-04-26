5f80edfab6387d6e1f5d01942f5899d6f9b2e389
“Erika <foglzerika@gmail.com>
2017-04-26 13:56:11 -0500

    Agregando evento a  todas las imagenes

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,7 @@ for( var i=0 ; i<imagenDetonador.length ; i++){
   console.log(imagenDetonador[i]);
   imagenDetonador[i].addEventListener("click",verVideo);
 }
+
+function verVideo(){
+  alert("Hola");
+}
