cf1e14a08c7f0836147899e0f566c977affb1836
“Erika <foglzerika@gmail.com>
2017-04-27 10:03:47 -0500

    Cambios al js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,18 @@
 var imagenDetonador= document.getElementsByClassName('imagenVideos');
 for( var i=0 ; i<imagenDetonador.length ; i++){
-  console.log(imagenDetonador[i]);
+
+  // if(i==0){
   imagenDetonador[i].addEventListener("click",verVideo);
+    // imagenDetonador[0].src="www.youtube.com";
+
 }
 
 function verVideo(){
-  alert("Hola");
+  var img = document.getElementById('myImageId');
+    img.style.visibility = 'hidden';
+  var video= document.getElementById("video1");
+  video.style.visibility="visible";
+  // <iframe dissable width="420" height="345" src="https://www.youtube.com/embed/XGSy3_Czz8k">
+  // </iframe>
+  // var nodoPadre = document.getElementsByClassName("")
 }
