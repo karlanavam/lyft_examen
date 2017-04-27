12b0ac8a75cbfe62bedfa38bee1d7fc8f6ba02f8
Your Name <you@example.com>
2017-04-27 09:44:52 -0500

    cambio Logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,28 @@
-document.addEventListener("mousemove",cambiarLogo);
-document.addEventListener("mousemove",apareceSingOn);
+document.addEventListener("scroll",cambiarLogo);
+document.addEventListener("scroll",apareceSingOn);
+document.addEventListener("scroll",fondoBarra);
 
 function cambiarLogo() {
   var logo = document.getElementById('imagenLogoCambio');
-  var header = document.getElementById('pruebaNav').offsetTop;
   var claseLogo = document.getElementById('nav');
 
-  logo.src = "assets/img/logo-white.png";
-  claseLogo.backgroundColor = "grey";
+  logo.src = "assets/img/logo-pink.png";
+
 
 
 }
 
 function apareceSingOn() {
   var sign = document.getElementById("sign");
-  console.log(sign);
-  sign.filter = "0%";
+
+
 
 
 }
 
-function barraNavColor() {
+function fondoBarra() {
+  var nav = document.getElementById('nav');
 
+    nav.style.backgroundColor = "white";
 
 }
