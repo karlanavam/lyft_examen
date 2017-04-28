342142533a532a024edf6c1620801c61358efe21
“Erika <foglzerika@gmail.com>
2017-04-28 09:48:51 -0500

    AGrega video pares

--- assets/js/videos.js/eventos.js
+++ assets/js/videos.js/eventos.js
@@ -4,7 +4,7 @@ var eventoClickIzquierda = document.getElementsByClassName("videoPlayIzquierda")
 var video= document.getElementsByClassName("play");
 eventoClick[0].addEventListener("click",reproduceVideo);
 eventoClick[1].addEventListener("click",reproduceVideo1);
-// eventoClickIzquierda[0].addEventListener("click",reproduceVideo2);
+eventoClickIzquierda[0].addEventListener("click",reproduceVideo2);
 
 
 function reproduceVideo(){
@@ -15,7 +15,7 @@ function reproduceVideo(){
   }
 
 function reproduceVideo1(){
-  eventoClick[2].style.display="none";
+  eventoClick[1].style.display="none";
   quitaPlay[2].style.display="none";
   video[2].style.visibility="visible";
   video[2].src+="&autoplay=1";
