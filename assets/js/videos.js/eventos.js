7bfd9aea2f391028d21f57904ec39a9e699674d4
“Erika <foglzerika@gmail.com>
2017-04-28 09:59:45 -0500

    Evento video listos

--- assets/js/videos.js/eventos.js
+++ assets/js/videos.js/eventos.js
@@ -1,6 +1,7 @@
 var eventoClick = document.getElementsByClassName("videoPlayDerecha");
 var eventoClickIzquierda = document.getElementsByClassName("videoPlayIzquierda");
  var quitaPlay=document.getElementsByClassName("imagenVideos");
+ var izquierda= document.getElementsByClassName("imagenVideosIzquierda");
 var video= document.getElementsByClassName("play");
 eventoClick[0].addEventListener("click",reproduceVideo);
 eventoClick[1].addEventListener("click",reproduceVideo1);
@@ -23,6 +24,8 @@ function reproduceVideo1(){
 }
 function reproduceVideo2(){
   // eventoClickIzquierda[0].style.display="none";
-  quitaPlay[2].style.display="none";
-
+   izquierda[0].style.display="none";
+  quitaPlay[1].style.display="none";
+  video[1].style.visibility="visible";
+  video[1].src+="&autoplay=1";
 }
